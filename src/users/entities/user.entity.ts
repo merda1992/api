887da9a99082519141a60957629ef7e5e0c3ad88
Yura <merda@tut.by>
2023-05-14 23:04:25 +0300

    userEntity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,26 +1,50 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
-import { Field, ID, ObjectType } from '@nestjs/graphql'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  Index,
+} from 'typeorm';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+export enum UserStatus {
+  Active = 'active',
+  Inactive = 'inactive',
+}
 
 @ObjectType()
 @Entity('users')
 export class UserEntity {
-	@Field(() => ID)
-	@PrimaryGeneratedColumn()
-	id: number
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Field()
-	@CreateDateColumn()
-	createdAt: Date
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
 
-	@Field()
-	@UpdateDateColumn()
-	updatedAt: Date
+  @Field()
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-	@Field()
-	@Column()
-	email: string
+  @Field()
+  @Column({ unique: true })
+  email: string;
 
-	@Field({ nullable: true })
-	@Column({ nullable: true })
-	name: string
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Field(() => String)
+  @Column({
+    type: 'enum',
+    enum: UserStatus,
+    default: UserStatus.Inactive,
+  })
+  @Index('IDX_USERS_STATUS')
+  status: UserStatus;
 }
